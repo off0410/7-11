bbe07f9390ab28a5ee04516259ff1088ae4e8606
off <147226097+off0410@users.noreply.github.com>
2025-07-08 15:06:10 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+	"go-netdisk/pkg/cmd"
+	"go-netdisk/pkg/version"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "go-netdisk"
+	app.Usage = "A simple net-disk service"
+	app.Version = version.Version
+	app.Commands = []*cli.Command{
+		cmd.Web,
+		cmd.Migrate,
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalf("Failed to start application: %v", err)
+	}
+}
